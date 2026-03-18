35a64ebcc82630cad18ae47818c0c40b21f228ed
Nihal-Tomar <nihaltomar95@gmail.com>
2026-03-18 16:35:58 +0530

    Initial commit

---
+++ admin_login.js
@@ -0,0 +1,5 @@
+let username = document.querySelector("#username") ;
+let password = document.querySelector("pwd") ;
+let login = document.querySelector("login") ;
+
+login.addEventListener("click", )
